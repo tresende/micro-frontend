ee6710b331d04d7c1df311bf10285fb127cd0364
Thiago Resende <thiago.gcresende@gmail.com>
2019-07-05 20:58:54 -0300

    rename

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {
     start
 } from 'single-spa';
 
-registerApplication('app1', () => import('./containers/app1/root.app.js'), () => true);
-registerApplication('app2', () => import('./containers/app2/root.app.js'), () => true);
-registerApplication('app3', () => import('./containers/app3/root.app.js'), () => true);
+registerApplication('app1', () => import('./containers/app1/index.js'), () => true);
+registerApplication('app2', () => import('./containers/app2/index.js'), () => true);
+registerApplication('app3', () => import('./containers/app3/index.js'), () => true);
 
 start();
 serviceWorker.register();
