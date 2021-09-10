cbd685807ad9e0fe6374397b5fbcf04c1b9f9ebb
Thiago Resende <thiago.gcresende@gmail.com>
2021-09-10 16:57:54 -0300

    💄 style: add css prefix name in production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'co' })
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Pricing from './components/Pricing'
 import Landing from './components/Landing'
 
+const generateClassName = createGenerateClassName({ productionPrefix: 'ma' })
+
 const App = () => {
   return (
     <>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route path="/pricing" component={Pricing} />
